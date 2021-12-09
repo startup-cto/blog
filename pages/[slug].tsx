f658f62c4e844a17e3bce12f8f7dccb7217e55b2
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-10 00:02:53 +0100

    feat: separate blog post content from meta data

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,6 +1,7 @@
 import { MDXRemote } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import * as fs from "fs";
+import matter from "gray-matter";
 
 export default function Post({ source }) {
   return <MDXRemote {...source} />;
@@ -14,10 +15,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps() {
-  const content = await fs.promises.readFile(
+  const file = await fs.promises.readFile(
     "./content/10-bad-typescript-habits-to-break-this-year.md",
     { encoding: "utf-8" }
   );
+  const { data, content } = matter(file);
   const source = await serialize(content);
   return { props: { source } };
 }
