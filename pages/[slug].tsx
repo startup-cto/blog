28538c56dcaacd494198fd1a77c19bb8028fe04f
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-09 23:57:36 +0100

    feat: render an article

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,15 +1,23 @@
 import { MDXRemote } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
+import * as fs from "fs";
 
 export default function Post({ source }) {
   return <MDXRemote {...source} />;
 }
 
 export async function getStaticPaths() {
-  return { paths: ["/test"], fallback: false };
+  return {
+    paths: ["/10-bad-typescript-habits-to-break-this-year"],
+    fallback: false,
+  };
 }
 
 export async function getStaticProps() {
-  const source = await serialize("# Title");
+  const content = await fs.promises.readFile(
+    "./content/10-bad-typescript-habits-to-break-this-year.md",
+    { encoding: "utf-8" }
+  );
+  const source = await serialize(content);
   return { props: { source } };
 }
