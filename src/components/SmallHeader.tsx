21aaacf907dae7926dbce14db8a0d521e2aa916a
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-12 00:27:20 +0100

    feat: use smaller avatar in small header

--- src/components/SmallHeader.tsx
+++ src/components/SmallHeader.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function SmallHeader() {
   return (
     <header>
-      <Image src={avatar} alt="The Startup CTO" />
+      <Image src={avatar} alt="The Startup CTO" width={100} height={100} />
       <Link href="/">Home</Link>
       <Link href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton">
         Follow on Twitter
