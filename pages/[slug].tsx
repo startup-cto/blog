e15d6640d8847acfe833646be4ff0cd0e3c04e91
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-11 23:02:30 +0100

    feat: add post meta and author information to posts

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,10 +2,11 @@ import { MDXRemote } from "next-mdx-remote";
 import { Head } from "../src/components/Head";
 import { loadPost, Post as PostType } from "../src/helpers/loadPost";
 import { loadPostFileNames } from "../src/helpers/loadPostFileNames";
-import { GetStaticProps } from "next";
 import Image from "next/image";
 import avatar from "../components/avatar.png";
 import Link from "next/link";
+import { GetStaticProps } from "next";
+import { Footer } from "../components/Footer";
 
 export default function Post({
   excerpt,
@@ -35,7 +36,26 @@ export default function Post({
           Follow on Twitter
         </Link>
       </header>
-      <MDXRemote {...source} />
+      <main>
+        {publishedAt && (
+          <span>{new Date(publishedAt).toLocaleDateString()}</span>
+        )}
+        {tags && <span>{tags.join(", ")}</span>}
+        <h1>{title}</h1>
+        <MDXRemote {...source} />
+      </main>
+      <aside>
+        <h1>Daniel Bartholomae</h1>
+        <p>
+          Daniel Bartholomae is a Berlin based founder and web developer. You
+          can{" "}
+          <Link href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton">
+            follow him on Twitter
+          </Link>
+          .
+        </p>
+      </aside>
+      <Footer />
     </>
   );
 }
