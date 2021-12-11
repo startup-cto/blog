82e990adeb9d97592fa2d945668d711c1cdaecec
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-11 22:58:56 +0100

    feat: add small header to post

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -3,6 +3,9 @@ import { Head } from "../src/components/Head";
 import { loadPost, Post as PostType } from "../src/helpers/loadPost";
 import { loadPostFileNames } from "../src/helpers/loadPostFileNames";
 import { GetStaticProps } from "next";
+import Image from "next/image";
+import avatar from "../components/avatar.png";
+import Link from "next/link";
 
 export default function Post({
   excerpt,
@@ -25,6 +28,13 @@ export default function Post({
         type="article"
         updatedAt={updatedAt ? new Date(updatedAt) : undefined}
       />
+      <header>
+        <Image src={avatar} alt="The Startup CTO" />
+        <Link href="/">Home</Link>
+        <Link href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton">
+          Follow on Twitter
+        </Link>
+      </header>
       <MDXRemote {...source} />
     </>
   );
