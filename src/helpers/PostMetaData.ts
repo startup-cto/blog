6fe5c08a64bda8b2eaccbdd50fbe939f7e58448e
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-11 21:43:28 +0100

    refactor: allow for date validation
    zod allows validating JavaScript dates, while ajv only allows JSON schema validation which store dates as strings. As gray-matter already converts these strings to dates, validation would fail otherwise.

--- src/helpers/PostMetaData.ts
+++ src/helpers/PostMetaData.ts
@@ -1,56 +1,18 @@
-import { Static, Type } from "@sinclair/typebox";
-import Ajv, { ErrorObject } from "ajv";
-import addFormats from "ajv-formats";
+import { z } from "zod";
 
-const ajv = addFormats(new Ajv(), [
-  "date-time",
-  "time",
-  "date",
-  "email",
-  "hostname",
-  "ipv4",
-  "ipv6",
-  "uri",
-  "uri-reference",
-  "uuid",
-  "uri-template",
-  "json-pointer",
-  "relative-json-pointer",
-  "regex",
-])
-  .addKeyword("kind")
-  .addKeyword("modifier");
-
-const postMetaDataSchema = Type.Object({
-  title: Type.String(),
-  slug: Type.String(),
-  publishedAt: Type.Optional(Type.String({ format: "date-time" })),
-  updatedAt: Type.Optional(Type.String({ format: "date-time" })),
-  tags: Type.Optional(Type.Array(Type.String())),
-  excerpt: Type.String(),
+const postMetaDataSchema = z.object({
+  title: z.string(),
+  slug: z.string(),
+  publishedAt: z.optional(z.date()),
+  updatedAt: z.optional(z.date()),
+  tags: z.optional(z.array(z.string())),
+  excerpt: z.string(),
 });
 
 export function assertPostMetaData(
   data: unknown
 ): asserts data is PostMetaData {
-  const isPostMetaData = ajv.compile(postMetaDataSchema);
-
-  if (isPostMetaData(data)) {
-    return;
-  }
-
-  throw new ValidationError(isPostMetaData.errors);
+  postMetaDataSchema.parse(data);
 }
 
-class ValidationError extends Error {
-  errors: ErrorObject[];
-
-  constructor(errors: ErrorObject[]) {
-    super(
-      "Validation failed:\n" + errors.map((error) => error.message).join("\n")
-    );
-    this.errors = errors;
-  }
-}
-
-export type PostMetaData = Static<typeof postMetaDataSchema>;
+export type PostMetaData = z.infer<typeof postMetaDataSchema>;
