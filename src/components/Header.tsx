3e9dd8a38718ec1a541470732861ef25c8c97957
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-13 20:14:09 +0100

    fix: use CSS modules for styling
    Inline styles clashed with TypeScript for next/image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,22 @@
 import Image from "next/image";
 import avatar from "./avatar.png";
 import { staticImageLoader } from "../helpers/StaticImageLoader";
+import styles from "./Header.module.css";
 
 export function Header() {
+  console.log(styles);
   return (
-    <header
-      style={{
-        backgroundColor: "#27232f",
-        color: "#dddbe6",
-        fontFamily: '"Courier New", Courier, monospace',
-        paddingTop: "100px",
-        textAlign: "center",
-      }}
-    >
+    <header className={styles.container}>
       <Image
         loader={staticImageLoader}
         src={avatar}
         alt="The Startup CTO"
         width={100}
         height={100}
-        style={{ borderRadius: "50%", margin: "0 auto" }}
+        className={styles.avatar}
       />
-      <h1 style={{ fontWeight: "normal" }}>The Startup CTO</h1>
-      <h2 style={{ fontWeight: "normal" }}>
-        Building companies with web technology
-      </h2>
+      <h1 className={styles.header}>The Startup CTO</h1>
+      <h2 className={styles.header}>Building companies with web technology</h2>
       <a href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton">
         Follow on Twitter
       </a>
