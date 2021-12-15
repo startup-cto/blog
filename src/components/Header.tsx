38c437e5bac7b900af970d330fb2fc48ffe91b18
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-15 22:42:44 +0100

    feat: finish Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,10 @@ export function Header() {
       />
       <h1 className={styles.header}>The Startup CTO</h1>
       <h2 className={styles.header}>Building companies with web technology</h2>
-      <a href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton">
+      <a
+        className={styles.link}
+        href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton"
+      >
         Follow on Twitter
       </a>
     </header>
