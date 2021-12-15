44c1a42eaf0d46ef4d8812a14518631d5ef3b975
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-15 23:30:51 +0100

    feat: format Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
+import styles from "./Footer.module.css";
 
 export function Footer() {
   return (
-    <footer>
+    <footer className={styles.container}>
       <div>
         All content copyright The Startup CTO © {new Date().getFullYear()} • All
         rights reserved.
       </div>
-      <ul>
-        <li>
+      <ul className={styles.nav}>
+        <li className={styles.navLink}>
           <Link href="/imprint">Imprint</Link>
         </li>
-        <li>
+        <li className={styles.navLink}>
           <Link href="/privacy-policy">Privacy policy</Link>
         </li>
       </ul>
