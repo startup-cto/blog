ce3902ccc25f33a044d86eff9bded595635e69ce
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-16 22:58:58 +0100

    feat: style SmallHeader

--- src/components/SmallHeader/SmallHeader.tsx
+++ src/components/SmallHeader/SmallHeader.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
 import avatar from "../avatar.png";
-import Link from "next/link";
+import styles from "./SmallHeader.module.css";
 
 export function SmallHeader() {
   return (
-    <header>
+    <header className={styles.container}>
       <Image
+        className={styles.avatar}
         src={avatar}
         unoptimized
         alt="The Startup CTO"
         width={100}
         height={100}
       />
-      <Link href="/">Home</Link>
-      <Link href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton">
+      <br />
+      <a className={styles.link} href="/">
+        Home
+      </a>
+      <a
+        className={styles.link}
+        href="https://twitter.com/intent/follow?original_referer=https%253A%252F%252Fstartup-cto.net%252F&ref_src=twsrc%5Etfw&region=follow_link&screen_name=The_Startup_CTO&tw_p=followbutton"
+      >
         Follow on Twitter
-      </Link>
+      </a>
     </header>
   );
 }
