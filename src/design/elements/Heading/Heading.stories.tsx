c9a11abd2de0dd22b882f69b4de7f34c0c71b3c8
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-21 23:03:50 +0100

    docs: show all Headings

--- src/design/elements/Heading/Heading.stories.tsx
+++ src/design/elements/Heading/Heading.stories.tsx
@@ -17,3 +17,13 @@ Default.args = {
   children: "Heading",
   variant: "h1",
 };
+
+export const All = () => (
+  <>
+    <Heading variant="h1">Heading 1</Heading>
+    <Heading variant="h2">Heading 2</Heading>
+    <Heading variant="h3">Heading 3</Heading>
+    <Heading variant="h4">Heading 4</Heading>
+    <Heading variant="h5">Heading 5</Heading>
+  </>
+);
