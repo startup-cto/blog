830a3cccd8da5bf65307d093217440d702fb3f5d
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-22 21:41:39 +0100

    refactor: add PublishedPost

---
+++ src/model/PublishedPost.test.ts
@@ -0,0 +1,17 @@
+import { isPublishedPost, PublishedPost } from "./PublishedPost";
+
+describe("PublishedPost", () => {
+  it("accepts a PublishedPost", () => {
+    const publishedPost: PublishedPost = {
+      title: "10 bad TypeScript habits to break this year",
+      slug: "10-bad-typescript-habits-to-break-this-year",
+      publishedAt: "2021-01-21T21:01:02.000Z",
+      updatedAt: "2021-02-13T10:40:00.000Z",
+      tags: ["TypeScript"],
+      excerpt:
+        "TypeScript and JavaScript have steadily evolved over the last years, and some of the habits we built over the last decades have become obsolete. Some might never have been meaningful. Here's a list of 10 habits that we all should break.",
+      previewImage: "/images/teaser/filename.png",
+    };
+    expect(isPublishedPost(publishedPost)).toBe(true);
+  });
+});
---
+++ src/model/PublishedPost.ts
@@ -0,0 +1,18 @@
+import { compile, TypeOf, v } from "suretype";
+
+const publishedPostSchema = v.object({
+  title: v.string().required(),
+  slug: v.string().required(),
+  publishedAt: v.string().format("date-time").required(),
+  updatedAt: v.string().format("date-time").required(),
+  tags: v.array(v.string()).required(),
+  excerpt: v.string().required(),
+  previewImage: v.string().format("uri-reference").required(),
+});
+
+export type PublishedPost = TypeOf<typeof publishedPostSchema>;
+
+export const isPublishedPost: (data: unknown) => data is PublishedPost =
+  compile(publishedPostSchema, {
+    simple: true,
+  });
