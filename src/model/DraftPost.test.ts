4835b5f2dd2d572c3f43340e28b96a140f14026e
Daniel Bartholomae <daniel@bartholomae.name>
2021-12-22 21:41:24 +0100

    refactor: add DraftPost

---
+++ src/model/DraftPost.test.ts
@@ -0,0 +1,25 @@
+import { DraftPost, isDraftPost } from "./DraftPost";
+
+describe("DraftPost", () => {
+  it("accepts a minimal DraftPost", () => {
+    const draftPost: DraftPost = {
+      draft: true,
+    };
+    expect(isDraftPost(draftPost)).toBe(true);
+  });
+
+  it("accepts a full DraftPost", () => {
+    const draftPost: DraftPost = {
+      draft: true,
+      title: "10 bad TypeScript habits to break this year",
+      slug: "10-bad-typescript-habits-to-break-this-year",
+      publishedAt: "2021-01-21T21:01:02.000Z",
+      updatedAt: "2021-02-13T10:40:00.000Z",
+      previewImage: "/images/teaser/filename.png",
+      tags: ["TypeScript"],
+      excerpt:
+        "TypeScript and JavaScript have steadily evolved over the last years, and some of the habits we built over the last decades have become obsolete. Some might never have been meaningful. Here's a list of 10 habits that we all should break.",
+    };
+    expect(isDraftPost(draftPost)).toBe(true);
+  });
+});
---
+++ src/model/DraftPost.ts
@@ -0,0 +1,21 @@
+import { compile, TypeOf, v } from "suretype";
+
+const draftPostSchema = v.object({
+  title: v.string(),
+  draft: v.boolean().const(true).required(),
+  slug: v.string(),
+  publishedAt: v.string().format("date-time"),
+  updatedAt: v.string().format("date-time"),
+  tags: v.array(v.string()),
+  excerpt: v.string(),
+  previewImage: v.string().format("uri-reference"),
+});
+
+export type DraftPost = TypeOf<typeof draftPostSchema>;
+
+export const isDraftPost: (data: unknown) => data is DraftPost = compile(
+  draftPostSchema,
+  {
+    simple: true,
+  }
+);
