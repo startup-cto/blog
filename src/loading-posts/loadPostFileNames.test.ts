d27ad1c44d94a47b04685a56b54cc61d0f8c0f98
Daniel Bartholomae <daniel@bartholomae.name>
2022-01-20 22:15:56 +0100

    test: loadPostFileNames loads file name

---
+++ src/loading-posts/loadPostFileNames.test.ts
@@ -0,0 +1,22 @@
+import { mockPostFiles, resetPostFiles } from "../test-helpers";
+import { PublishedPostMock } from "../data-structure/PublishedPost/PublishedPostMock";
+import { loadPostFileNames } from "./loadPostFileNames";
+
+describe("loadPostFileNames", () => {
+  describe("with one post", () => {
+    const postFiles = [new PublishedPostMock()];
+
+    beforeAll(() => {
+      mockPostFiles(postFiles);
+    });
+
+    afterAll(() => {
+      resetPostFiles();
+    });
+
+    it("loads file name of the post", async () => {
+      const posts = await loadPostFileNames();
+      expect(posts).toContainEqual(`/${postFiles[0].slug}`);
+    });
+  });
+});
