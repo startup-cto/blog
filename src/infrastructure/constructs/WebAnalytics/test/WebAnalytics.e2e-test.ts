3cc0a7fc67fac0ff64320ae959e741bd1995aaf0
Daniel Bartholomae <daniel@bartholomae.name>
2022-03-13 11:12:28 +0100

    test: new analytics events are actually created

--- src/infrastructure/constructs/WebAnalytics/test/WebAnalytics.e2e-test.ts
+++ src/infrastructure/constructs/WebAnalytics/test/WebAnalytics.e2e-test.ts
@@ -1,14 +1,20 @@
 import fetch from "node-fetch";
+import { v4 as uuid } from "uuid";
 
 const url = process.env.WEB_ANAlYTICS_URL!;
 
 describe("WebAnalytics", () => {
   it("returns an event that was just created", async () => {
-    const path = "/test-url";
-    await fetch(url, {
+    const path = `/test-url/${uuid()}`;
+    const postResponse = await fetch(url, {
       method: "POST",
       body: JSON.stringify({ url: path }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    expect(postResponse.ok).toBe(true);
 
     const response = await fetch(url);
     expect(await response.json()).toContainEqual(
