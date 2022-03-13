a6150970008f9c3a69fd251155255871d171d2bd
Daniel Bartholomae <daniel@bartholomae.name>
2022-03-13 15:59:18 +0100

    feat: track pageviews

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import "normalize.css";
 import styles from "../src/presentation/base.module.css";
 import { AppProps } from "next/app";
+import { useTrackPageView } from "../src/analytics/useTrackPageView";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useTrackPageView();
+
   return (
     <div className={styles.base}>
       <Component {...pageProps} />
