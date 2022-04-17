0322c99441434fbe9e17b42ea3caaa95c8a26246
Daniel Bartholomae <daniel@bartholomae.name>
2022-04-17 16:52:38 +0200

    Improve linting for access package

--- access/.eslintrc.js
+++ access/.eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
-  extends: ["prettier"],
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:jest-formatting/strict",
+    "prettier",
+  ],
+  rules: {
+    // This rule would disallow using async to make the result a promise
+    "@typescript-eslint/require-await": "off",
+    // Enable auto-fixing for prettier rules
+    "prettier/prettier": "error"
+  },
 };
