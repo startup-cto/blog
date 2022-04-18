f20d76da14ebc24ba66aca2faf2cd745b3f3e05f
Daniel Bartholomae <daniel@bartholomae.name>
2022-04-18 13:56:06 +0200

    Add linting to blog

--- blog/.eslintrc.js
+++ blog/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier", "plugin:storybook/recommended"],
+  plugins: ["prettier"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:jest-formatting/strict",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
+  rules: {
+    // Enable auto-fixing for prettier rules
+    "prettier/prettier": "error",
+  },
 };
