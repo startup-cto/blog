197a0dc107fa8be01b0a7a307fbc0d3a4a96ffa6
Daniel Bartholomae <daniel@bartholomae.name>
2022-04-30 23:47:34 +0200

    Allow to deploy stage manually

---
+++ access/bin/pipeline.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import { App } from "aws-cdk-lib";
+import { PipelineStack } from "../src/PipelineStack";
+import { synthZipFileName } from "../config/synthZipFileName";
+import { githubDeploymentRoleName } from "../config/githubDeploymentRoleName";
+import { AccessStage } from "../src/AccessStage";
+import { Construct } from "constructs";
+
+const app = new App();
+
+new PipelineStack(app, "AccessPipelineStack", {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "eu-central-1" },
+  deploymentRoleName: githubDeploymentRoleName,
+  stageFactory: (scope: Construct) => new AccessStage(scope, "AccessStage"),
+  synthZipFileName: synthZipFileName,
+});
+
+app.synth();
