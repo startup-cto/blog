8633152894c4a6cabe29d440a57fe380be6a38d3
Daniel Bartholomae <daniel@bartholomae.name>
2022-05-01 18:32:48 +0200

    Extract S3PipelineStack into its own module

--- access/bin/pipeline.ts
+++ access/bin/pipeline.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import { App } from "aws-cdk-lib";
-import { PipelineStack } from "../src/PipelineStack";
 import { synthZipFileName } from "../config/synthZipFileName";
 import { githubDeploymentRoleName } from "../config/githubDeploymentRoleName";
 import { AccessStage } from "../src/AccessStage";
 import { Construct } from "constructs";
+import { S3PipelineStack } from "s3-pipeline-stack";
 
 const app = new App();
 
-new PipelineStack(app, "AccessPipelineStack", {
+new S3PipelineStack(app, "AccessPipelineStack", {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "eu-central-1" },
   deploymentRoleName: githubDeploymentRoleName,
   stageFactory: (scope: Construct) => new AccessStage(scope, "AccessStage"),
