79f89a97aa7d5839564e6d2dde55ad84502ffa5e
Daniel Bartholomae <daniel@bartholomae.name>
2022-11-13 22:49:57 +0100

    Add paginated overview pages

---
+++ blog/src/pages/page/[pageNumber].tsx
@@ -0,0 +1,34 @@
+import { GetStaticProps } from "next";
+import { Home, Props } from "../../presentation/templates/Home/Home";
+import { loadPostSummaries } from "../../loading-posts/loadPostSummaries";
+import { loadPostFileNames } from "../../loading-posts/loadPostFileNames";
+
+export default Home;
+
+const pageSize = 10;
+
+export async function getStaticPaths() {
+  const names = await loadPostFileNames();
+  const pages = Math.ceil(names.length / pageSize);
+  return {
+    paths: Array.from({ length: pages }).map(
+      (_, index) => `/page/${index + 1}`
+    ),
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps<
+  Props,
+  { pageNumber: string }
+> = async ({ params }) => {
+  const pageNumber = Number.parseInt(params?.pageNumber ?? "1");
+  const posts = await loadPostSummaries();
+  const postsOnPage = posts.slice(
+    (pageNumber - 1) * pageSize,
+    pageNumber * pageSize
+  );
+  return {
+    props: { posts: postsOnPage },
+  };
+};
