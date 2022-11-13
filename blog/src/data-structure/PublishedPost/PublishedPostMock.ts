bf91b42ffe23cb96737a8c0e68956cc57380a332
Daniel Bartholomae <daniel@bartholomae.name>
2022-11-13 23:07:56 +0100

    Create unique filenames in PublishedPostMock

--- blog/src/data-structure/PublishedPost/PublishedPostMock.ts
+++ blog/src/data-structure/PublishedPost/PublishedPostMock.ts
@@ -7,8 +7,8 @@ export class PublishedPostMock implements PublishedPost {
     'Nowadays, only a few professional developers are left that seriously doubt the value of test-driven-development and test-driven-design (tdd). But the reality of many codebases I have seen is that tdd is often limited to the backend, where the "business logic" lives...';
   excerpt: string =
     "Only few professional developers seriously doubt the value of tdd. But in reality, tdd is often limited to the backend. Part of it is due to missing skills on how to tdd in the frontend.";
-  previewImage: string = "/images/teaser/tdd-in-a-react-frontend.png";
-  slug: string = "tdd-in-a-react-frontend";
+  previewImage: string = `/images/teaser/tdd-in-a-react-frontend-${uuid()}.png`;
+  slug: string = `tdd-in-a-react-frontend-${uuid()}`;
   tags: string[] = ["React", "tdd"];
   title: string = "TDD in a React frontend";
   publishedAt: string = "2021-01-19T22:32:26.000Z";
