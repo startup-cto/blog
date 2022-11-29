0a3f41de230d8c42e30a86cc97d74ee1cd4cf495
Daniel Bartholomae <daniel@bartholomae.name>
2022-11-29 22:38:02 +0100

    Publish new articles on dev.to

--- blog/scripts/publishPosts.ts
+++ blog/scripts/publishPosts.ts
@@ -4,8 +4,13 @@ import { loadToPublishPosts } from "../src/loading-posts/loadToPublishPosts";
 import { ToPublishPost } from "../src/data-structure/ToPublishPost/ToPublishPost";
 import { toPostFile } from "../src/loading-posts/toPostFile/toPostFile";
 import { simpleGit } from "simple-git";
+import { DevToPublisher } from "./dev-to/DevToPublisher";
 
 async function publishPosts() {
+  if (!process.env.DEV_TO_API_KEY) {
+    throw new TypeError("DEV_TO_API_KEY not set");
+  }
+
   const postsToPublish = await loadToPublishPosts();
   const postCount = postsToPublish.length;
   if (postCount === 0) {
@@ -17,7 +22,7 @@ async function publishPosts() {
     `Publishing ${postCount} post${postCount > 1 ? "s" : ""} at ${now}`
   );
 
-  await updatePosts(postsToPublish, now);
+  const updatedPosts = await updatePosts(postsToPublish, now);
   const git = simpleGit();
   console.log("Preparing commit...");
   await git.add(
@@ -28,20 +33,34 @@ async function publishPosts() {
   console.log("Preparing push...");
   await git.push();
   console.log("Changes pushed");
+
+  console.log("Publishing post to dev.to...");
+  const devToPublisher = new DevToPublisher(
+    "https://dev.to",
+    process.env.DEV_TO_API_KEY
+  );
+  await Promise.all(
+    updatedPosts.map((post) => devToPublisher.publishPost(post))
+  );
 }
 
 async function updatePosts(
   postsToPublish: ToPublishPost[],
   publishDate: string
 ) {
+  const updatedPosts = postsToPublish.map((post) => ({
+    ...post,
+    publishedAt: publishDate,
+  }));
   await Promise.all(
-    postsToPublish.map(async (post) => {
+    updatedPosts.map(async (post) => {
       await writeFile(
         join("content", `${post.slug}.md`),
-        toPostFile({ ...post, publishedAt: publishDate }, post.content)
+        toPostFile(post, post.content)
       );
     })
   );
+  return updatedPosts;
 }
 
 publishPosts()
