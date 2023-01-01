9e8d9da71b064b60c12aa3cf8fdd384a58a3d01a
Daniel Bartholomae <daniel@bartholomae.name>
2023-01-01 14:40:59 +0100

    Break long lines of inline code

--- blog/src/presentation/elements/InlineCode/InlineCode.stories.tsx
+++ blog/src/presentation/elements/InlineCode/InlineCode.stories.tsx
@@ -16,3 +16,9 @@ export const Default = Template.bind({});
 Default.args = {
   children: "some code",
 };
+
+export const ReallyLong = Template.bind({});
+ReallyLong.args = {
+  children:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id metus a enim laoreet lobortis vitae vel tortor. Phasellus mollis turpis non tortor vestibulum maximus. Morbi id aliquam mi.",
+};
