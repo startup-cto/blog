42279554ef8dad0a9ad0914a282e1d7c9730daa0
Daniel Bartholomae <daniel@bartholomae.name>
2024-12-29 18:16:30 +0100

    Make hiring links pretty

--- blog/src/presentation/blocks/Header/Header.tsx
+++ blog/src/presentation/blocks/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from "../../elements/Avatar/Avatar";
 import { Heading } from "../../elements/Heading/Heading";
 import { Box } from "../../elements/Box/Box";
+import { Link } from "../../elements/Link/Link";
 
 export function Header() {
   return (
@@ -9,7 +10,9 @@ export function Header() {
       <Heading variant="h1">The Startup CTO</Heading>
       <Heading variant="h2">Building companies with web technology</Heading>
       <Heading variant="h3">
-        <a href="mailto:daniel@startup-cto.net">Hire me as a consultant</a>
+        <Link href="mailto:daniel@startup-cto.net">
+          Hire me as a consultant
+        </Link>
       </Heading>
     </Box>
   );
